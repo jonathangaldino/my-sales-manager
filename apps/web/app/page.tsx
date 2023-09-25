c0e4057339af103804e4588f89f96ac0304c6080
Jonathan Galdino <johnzballad@gmail.com>
2023-09-25 13:51:29 -0300

    build: from pnpm to yarn (old but gold)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,8 @@
-import { prisma } from "@packages/database";
-
 export default async function IndexPage() {
-  const users = await prisma.user.findMany();
-
   return (
     <div>
       <h1>Hello World</h1>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <pre>sup</pre>
     </div>
   );
 }
