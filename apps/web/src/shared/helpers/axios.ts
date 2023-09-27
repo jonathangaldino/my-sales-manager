21251c68a81fddcac4b05a598ca123f2dac9f6c1
Jonathan Galdino <johnzballad@gmail.com>
2023-09-27 03:47:31 -0300

    feat/web: organizing some stuffs + envs

--- apps/web/src/shared/helpers/axios.ts
+++ apps/web/src/shared/helpers/axios.ts
@@ -1,11 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Set config defaults when creating the instance
 const reqInstance = axios.create({
-  baseURL: 'http://localhost:3000',
-  headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:3001'
-  }
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000",
+  headers: {},
 });
 
-export default reqInstance
+export default reqInstance;
