d83332e6ab6df45613221a21a5c858096070ff0a
Jonathan Galdino <johnzballad@gmail.com>
2023-09-27 02:22:29 -0300

    feat/web: improving ui (dashboard)

--- apps/web/src/modules/dashboard/components/TransactionsAnalytics.component.tsx
+++ apps/web/src/modules/dashboard/components/TransactionsAnalytics.component.tsx
@@ -62,33 +62,28 @@ const TransactionsAnalytics = ({ analytics, isLoading }: Props) => {
   const analyticsArr = createAnalyticsArr(analytics);
 
   return (
-    <>
-      <div className="m-10 grid gap-5 sm:grid-cols-3  mx-auto max-w-screen-lg">
-        {analyticsArr.map(({ description, value, color }) => (
-          <div
-            className="px-4 py-6 shadow-lg shadow-blue-100"
-            key={description}
+    <div className="m-10 grid gap-5 sm:grid-cols-4  mx-auto max-w-screen-lg">
+      {analyticsArr.map(({ description, value, color }) => (
+        <div className="px-4 py-6 shadow-lg shadow-blue-100" key={description}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`h-14 w-14 rounded-xl bg-${color}-400 p-4 text-white`}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={`h-14 w-14 rounded-xl bg-${color}-400 p-4 text-white`}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <p className="mt-4 font-medium">{description}</p>
-            <p className="mt-2 text-xl font-medium">{value}</p>
-          </div>
-        ))}
-      </div>
-    </>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <p className="mt-4 font-medium">{description}</p>
+          <p className="mt-2 text-xl font-medium">{value}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 
--- apps/web/src/modules/dashboard/components/TransactionsTable.component.tsx
+++ apps/web/src/modules/dashboard/components/TransactionsTable.component.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import Loading from "../../../shared/components/Loading.component";
 import { Transaction } from "../../../shared/types";
 
@@ -19,28 +20,34 @@ function generateOffsetArray(totalPages: number): number[] {
   return result;
 }
 
-const TransactionsTable = (props: Props) => {
-  const pages = generateOffsetArray(props.totalPages);
+const TransactionsTable = ({
+  totalPages,
+  currentPage,
+  changePageFn,
+  loading,
+  transactions,
+}: Props) => {
+  const pages = useMemo(() => generateOffsetArray(totalPages), [totalPages]);
 
-  if (props.loading) {
+  if (loading) {
     return <Loading />;
   }
 
-  const isCurrentPage = (page: number) => page === props.currentPage;
+  const isCurrentPage = (page: number) => page === currentPage;
   const isCurrentPageCssLoader = (page: number) =>
     isCurrentPage(page)
       ? `bg-blue-50 text-blue-600 hover:bg-blue-100 hover:text-blue-700 `
       : ``;
 
   const handlePagination = (page: number) => {
-    if (page === props.currentPage) return;
+    if (page === currentPage) return;
 
-    props.changePageFn(page);
+    changePageFn(page);
   };
 
   return (
-    <div className="relative overflow-x-auto">
-      <nav aria-label="Page navigation example">
+    <div className="overflow-x-auto flex flex-col items-center justify-center h-full">
+      <nav aria-label="Table navigation" className="mb-2">
         <ul className="inline-flex -space-x-px text-sm">
           {pages.map((pageNumber) => (
             <li key={`page-${pageNumber + 1}`}>
@@ -58,61 +65,60 @@ const TransactionsTable = (props: Props) => {
         </ul>
       </nav>
 
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Tipo
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Data
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Produto
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Valor
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Vendedor
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Descrição
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Natureza
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Sinal
-            </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {props.transactions.map((transaction) => (
-            <tr
-              key={transaction.id}
-              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-            >
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {transaction.type}
+      {transactions.length ? (
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Descrição
               </th>
-              <td className="px-6 py-4">{transaction.date}</td>
-              <td className="px-6 py-4">{transaction.product}</td>
-              <td className="px-6 py-4">{transaction.amount}</td>
-              <td className="px-6 py-4">{transaction.seller}</td>
-              <td className="px-6 py-4">{transaction.description}</td>
-              <td className="px-6 py-4">{transaction.nature}</td>
-              <td className="px-6 py-4">{transaction.signal}</td>
-            </tr>
-          ))}
-        </tbody>
+              <th scope="col" className="px-6 py-3">
+                Data
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Produto
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Valor
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Vendedor
+              </th>
 
-        {!props.transactions.length && <div>No data to show.</div>}
-      </table>
+              <th scope="col" className="px-6 py-3">
+                Natureza
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Sinal
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr
+                key={transaction.id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {transaction.description}
+                </th>
+                <td className="px-6 py-4">{transaction.date}</td>
+                <td className="px-6 py-4">{transaction.product}</td>
+                <td className="px-6 py-4">{transaction.amount}</td>
+                <td className="px-6 py-4">{transaction.seller}</td>
+                <td className="px-6 py-4">{transaction.nature}</td>
+                <td className="px-6 py-4">{transaction.signal}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-gray-500 dark:text-gray-400">No data to show.</p>
+      )}
     </div>
   );
 };
